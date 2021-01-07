b6cbf936e19eea47b69d1243d0e4ca106822a031
AniketGujar <gujaraniket7875@gmail.com>
2021-01-07 16:03:08 +0530

    Added: Code to check Employee Attendence

--- employeeWageComputation.js
+++ employeeWageComputation.js
@@ -1,1 +1,14 @@
 console.log("Welcome to Employee Wage Computation Program");
+
+function attendence() {
+    let employee_Attendence = Math.floor(Math.random() * 2);
+
+    if (employee_Attendence == 0) {
+        console.log("Employee is Present");
+    }
+    else {
+        console.log("Employee is Present");
+    }
+}
+
+attendence();
