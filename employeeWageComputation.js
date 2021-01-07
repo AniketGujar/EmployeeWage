de1f7e5c51edfaa66f9bc53bcd65b6694e684434
AniketGujar <gujaraniket7875@gmail.com>
2021-01-07 17:01:08 +0530

    Added: Wage Calcualtion using Switch Case

--- employeeWageComputation.js
+++ employeeWageComputation.js
@@ -6,14 +6,16 @@ const PART_TIME_WORKING_HOURS = 4;
 function attendence() {
     let employee_Attendence = Math.floor(Math.random() * 3);
 
-    if (employee_Attendence == 0) {
-        console.log("Employee is Present \t Daily Wage = " + FULL_DAY_WAGE * FULL_DAY_WORKING_HOURS);
-    }
-    else if (employee_Attendence == 1) {
-        console.log("Employee is Present Part Time \t Daily Wage = " + FULL_DAY_WAGE * PART_TIME_WORKING_HOURS);
-    }
-    else {
-        console.log("Employee is Absent \t Daily Wage = 0");
+    switch (employee_Attendence) {
+        case 0:
+            console.log("Employee is Present \t Daily Wage = " + FULL_DAY_WAGE * FULL_DAY_WORKING_HOURS);
+            break;
+        case 1:
+            console.log("Employee is Present Part Time \t Daily Wage = " + FULL_DAY_WAGE * PART_TIME_WORKING_HOURS);
+            break;
+        case 2:
+            console.log("Employee is Absent \t Daily Wage = 0");
+            break;
     }
 }
 
