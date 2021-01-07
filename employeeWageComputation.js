fedc5f40004b476f20841f77522c1e9631b87eda
AniketGujar <gujaraniket7875@gmail.com>
2021-01-07 16:45:21 +0530

    Added: Calculation for part time emoployee wage

--- employeeWageComputation.js
+++ employeeWageComputation.js
@@ -1,15 +1,19 @@
 console.log("Welcome to Employee Wage Computation Program");
-const FULL_DAY_WAGE=20;
-const FULL_DAY_WORKING_HOURS=8;
+const FULL_DAY_WAGE = 20;
+const FULL_DAY_WORKING_HOURS = 8;
+const PART_TIME_WORKING_HOURS = 4;
 
 function attendence() {
-    let employee_Attendence = Math.floor(Math.random() * 2);
+    let employee_Attendence = Math.floor(Math.random() * 3);
 
     if (employee_Attendence == 0) {
-        console.log("Employee is Present \t Daily Wage = "+ FULL_DAY_WAGE*FULL_DAY_WORKING_HOURS);
+        console.log("Employee is Present \t Daily Wage = " + FULL_DAY_WAGE * FULL_DAY_WORKING_HOURS);
+    }
+    else if (employee_Attendence == 1) {
+        console.log("Employee is Present Part Time \t Daily Wage = " + FULL_DAY_WAGE * PART_TIME_WORKING_HOURS);
     }
     else {
-        console.log("Employee is Absent");
+        console.log("Employee is Absent \t Daily Wage = 0");
     }
 }
 
