2b33a3a9b55af40a6529649ea34792d0647c5541
AniketGujar <gujaraniket7875@gmail.com>
2021-01-07 18:05:13 +0530

    Added: Code to store Daily and Monthly Wage

--- employeeWageComputation.js
+++ employeeWageComputation.js
@@ -1,12 +1,13 @@
 console.log("Welcome to Employee Wage Computation Program");
 
-const FULL_DAY_WAGE = 20;
+const HOUR_WAGE = 20;
 const FULL_DAY_WORKING_HOURS = 8;
 const PART_TIME_WORKING_HOURS = 4;
 const MAX_WORKING_HOURS = 100;
 const MAX_WORKING_DAYS = 20
 let totalWorkingHours = 0;
 let totalWorkingDays = 0;
+let totalWage=0;
 
 function attendence() {
 
@@ -16,12 +17,14 @@ function attendence() {
 
         switch (employee_Attendence) {
             case 0:
-                console.log("Employee is Present \t\t Daily Wage = " + FULL_DAY_WAGE * FULL_DAY_WORKING_HOURS);
+                console.log("Employee is Present \t\t Daily Wage = " + HOUR_WAGE * FULL_DAY_WORKING_HOURS);
                 totalWorkingHours += FULL_DAY_WORKING_HOURS;
+                totalWage+=HOUR_WAGE * FULL_DAY_WORKING_HOURS;
                 break;
             case 1:
-                console.log("Employee is Present Part Time \t Daily Wage = " + FULL_DAY_WAGE * PART_TIME_WORKING_HOURS);
+                console.log("Employee is Present Part Time \t Daily Wage = " + HOUR_WAGE * PART_TIME_WORKING_HOURS);
                 totalWorkingHours += PART_TIME_WORKING_HOURS;
+                totalWage+=HOUR_WAGE* PART_TIME_WORKING_HOURS;
                 break;
             case 2:
                 console.log("Employee is Absent \t\t Daily Wage = 0");
@@ -34,3 +37,4 @@ attendence();
 
 console.log("Total Working Hours = " + totalWorkingHours);
 console.log("Total Working Days = " + totalWorkingDays);
+console.log("Total Monthly Wage = " + totalWage);
